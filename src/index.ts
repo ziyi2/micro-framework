be5aa9a49f61482bfe70b4ae62475bcb38b3d69b
ziyi2 <18768107826@163.com>
2023-03-12 09:20:20 +0800

    feat: 新增 Prettier 配置

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,3 @@ export * from "./opt/opt2";
 export * from "./sandbox/sandbox1";
 export * from "./sandbox/sandbox2";
 export * from "./sandbox/sandbox3";
-
-export function a(): string {
-  // 没有分号
-  return "a";
-}
-
-export function a(): number {
-  const a = 1;
-  const b = 2;
-  return a + b;
-}
