6f8afe09436ff87914c6071abf045594f4d6c431
ziyi2 <18768107826@163.com>
2023-05-24 19:57:54 +0800

    feat: iframe 隔离

--- micro-server.js
+++ micro-server.js
@@ -1,13 +1,22 @@
 // micro-server.js
 import express from "express";
 import morgan from "morgan";
-import path from 'path';
+import path from "path";
 import config from "./config.js";
 const app = express();
+
 const { port, host } = config;
 
 // 打印请求日志
 app.use(morgan("dev"));
+
+// 设置支持跨域请求头
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Allow", "GET, POST, OPTIONS");
+  next();
+});
 
 app.use(
   express.static(path.join("public", "micro"), {
--- public/micro/micro2.js
+++ public/micro/micro2.js
@@ -1,17 +1,6 @@
-// 立即执行的匿名函数可以防止变量 root 产生冲突
-(function () {
-  let root;
-
-  window.micro2_mount = function (el) {
-    // 以下其实可以是 React 框架或者 Vue 框架生成的 Document 元素，这里只是做一个简单的示例
-    root = document.createElement("h1");
-    root.textContent = "微应用2";
-    const $el = document.querySelector(el);
-    $el?.appendChild(root);
-  };
-
-  window.micro2_unmount = function () {
-    if (!root) return;
-    root.parentNode?.removeChild(root);
-  };
-})();
+let root;
+// 以下其实可以是 React 框架或者 Vue 框架生成的 Document 元素，这里只是做一个简单的示例
+root = document.createElement("h1");
+root.textContent = "微应用2";
+// 约定往空的 iframe 的 body 下挂载微应用
+document.body.appendChild(root);
