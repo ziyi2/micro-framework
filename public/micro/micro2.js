714404713b65dbf93ceabce14e6e50b4ea4e5897
ziyi2 <18768107826@163.com>
2023-06-27 09:27:06 +0800

    feat: 完善微前端沙箱

--- public/micro/micro2.js
+++ public/micro/micro2.js
@@ -4,3 +4,11 @@ root = document.createElement("h1");
 root.textContent = "微应用2";
 // 约定往空的 iframe 的 body 下挂载微应用
 document.body.appendChild(root);
+
+this.a = 333;
+console.log('a: ', a);
+console.log('a: ', window.a);
+
+var b = 444;
+console.log('b: ', b);
+console.log('b: ', window.b);
