7ae54845738590ab1c97117d99e9f795a7e7494f
ziyi2 <18768107826@163.com>
2023-07-11 20:19:10 +0800

    feat: 同域通信

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# iframe 同源隔离
+# 主子应用同域通信
 
 ``` bash
 # 启动主应用服务
@@ -9,7 +9,3 @@ npm run micro
 
 启动之后访问主应用地址
 
-
-## 测试 Web 框架
-
-如果想要测试 Vue 或者 React 框架是否可以在隔离的 iframe 中生效，可以使用 [test/iframe-sandbox-micro-app](https://github.com/ziyi2/micro-framework/tree/test/iframe-sandbox-micro-app) 分支进行代码的修改和构建，构建后将 UMD 规范的产物放入 `public/micro` 目录的 `micro1.js` 中进行测试。
--- public/micro/micro2.js
+++ public/micro/micro2.js
@@ -1,16 +1,9 @@
-this.a = 1;
-console.log("微应用2 a: ", a);
+window.parent.addEventListener("mainChannel", (e) => {
+  console.log("micro2 addEventListener: ", e);
+});
 
-var b = 2;
-console.log("微应用2 b: ", window.b);
-
-window.c = 3;
-console.log("微应用2 c: ", window.c);
-
-let root = document.createElement("button");
-root.textContent = "微应用 2 更改 history 为 micro2";
-document.body.appendChild(root);
-
-root.onclick = () => {
-  history.pushState({}, "", "/micro2");
-};
+window.parent.dispatchEvent(
+  new CustomEvent("microChannel", {
+    detail: "micro2",
+  })
+);
