be31fc2a72c918826c550da6b03e821422110e1f
ziyi2 <18768107826@163.com>
2023-07-24 19:56:29 +0800

    feat: 设置 Expires 缓存

--- micro-server.js
+++ micro-server.js
@@ -1,7 +1,7 @@
 // micro-server.js
 import express from "express";
 import morgan from "morgan";
-import path from 'path';
+import path from "path";
 import config from "./config.js";
 const app = express();
 const { port, host } = config;
@@ -11,16 +11,16 @@ app.use(morgan("dev"));
 
 app.use(
   express.static(path.join("public", "micro"), {
-    // 缓存技术有很多种，包括 CDN 缓存、DNS 缓存、Nginx 缓存、代理缓存、服务端缓存和浏览器缓存...
-    // 浏览器缓存又有多种类型：HTTP 缓存、内存缓存、硬盘缓存、Prefetch 缓存、Service Worker 缓存、Push 缓存...
-    // 这里设置的是浏览器缓存中的 HTTP 缓存
-    // 设置静态资源的缓存，可以结合协商缓存
-    // maxAge: 86400000, // 强缓存
-    etag: true,
-    lastModified: true,
+    cacheControl: false,
+    etag: false,
+    lastModified: false,
+    setHeaders: (res) => {
+      // 10 秒后强缓存失效
+      res.set("Expires", new Date(Date.now() + 5 * 1000).toGMTString());
+    },
   })
 );
 
 // 启动 Node 服务
 app.listen(port.micro, host);
-console.log(`server start at http://${host}:${port.main}/`);
+console.log(`server start at http://${host}:${port.micro}/`);
