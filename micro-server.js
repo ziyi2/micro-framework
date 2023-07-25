e400ee8a82f870c3d41e2d682e341fb102fa7bcb
ziyi2 <18768107826@163.com>
2023-07-25 19:44:13 +0800

    feat: 修改注释

--- micro-server.js
+++ micro-server.js
@@ -13,11 +13,10 @@ app.use(
   express.static(path.join("public", "micro"), {
     // 禁用 cache-control，HTTP / 1.1 的缓存能力
     cacheControl: false,
-    // 禁用协商缓存
     etag: false,
     lastModified: false,
     setHeaders: (res) => {
-      // 5 秒后强缓存失效，注意使用 GMT 格式时间
+      // 5 秒后缓存失效，注意使用 GMT 格式时间
       res.set("Expires", new Date(Date.now() + 5 * 1000).toGMTString());
     },
   })
