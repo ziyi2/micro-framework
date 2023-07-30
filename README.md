93b52e9a6a5768712b2b82f809402bab1a4cd455
ziyi2 <18768107826@163.com>
2023-07-30 10:01:04 +0800

    feat: 修改示例

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # 站点隔离测试
 
 ``` bash
-node main-server.js
-# 打开新的终端执行
-node micro-server.js
+# 安装依赖
+npm i
+# 启动主应用服务
+npm run main-start
+# 启动微应用服务（打开新的终端执行）
+npm run micro-start
 ```
--- main-server.js
+++ main-server.js
@@ -16,7 +16,7 @@ app.set("view engine", "html");
 
 // 浏览器访问 http://${host}:${port.main}/ 时会渲染 views/main.html 
 app.get("/", function (req, res) {
-  // 使用 ejs 模版引擎填充主应用 views/main.html 中的 micro 变量，并将其渲染到浏览器
+  // 使用 ejs 模版引擎填充主应用 views/main.html 中的 iframeUrl 变量，并将其渲染到浏览器
   res.render("main", {
     // 填充 iframe 应用的地址，只有端口不同，iframe 应用和 main 应用跨域但是同站
     iframeUrl: `http://${host}:${port.micro}`
