0c901d0753ca636202d0db388c338014e63ccdeb
ziyi2 <18768107826@163.com>
2023-08-13 10:32:41 +0800

    feat: 优化示例

--- public/micro/micro1.js
+++ public/micro/micro1.js
@@ -1,26 +1,19 @@
-let root;
-root = document.createElement("h1");
-root.textContent = "微应用1";
+this.a = 'a';
+console.log('微应用1 a: ', a);
+
+var b = 'b';
+console.log('微应用1 b: ', window.b);
+
+window.c = 'c';
+console.log('微应用1 c: ', window.c);
+
+let root = document.createElement("button");
+root.textContent = "微应用 1 更改 history 为 micro1";
 document.body.appendChild(root);
 
-
-// 打开以下注释测试隔离
-// this.a = 'a';
-// console.log('微应用1 a: ', a);
-
-// var b = 'b';
-// console.log('微应用1 b: ', window.b);
-
-// window.c = 'c';
-// console.log('微应用1 c: ', window.c);
-
-// let root = document.createElement("button");
-// root.textContent = "微应用 1 更改 history 为 micro1";
-// document.body.appendChild(root);
-
-// root.onclick = () => {
-//   history.pushState({}, '', '/micro1');
-// }
+root.onclick = () => {
+  history.pushState({}, '', '/micro1');
+}
 
 
 
--- public/micro/micro2.js
+++ public/micro/micro2.js
@@ -1,21 +1,16 @@
-let root;
-root = document.createElement("h1");
-root.textContent = "微应用2";
+this.a = 1;
+console.log("微应用2 a: ", a);
+
+var b = 2;
+console.log("微应用2 b: ", window.b);
+
+window.c = 3;
+console.log("微应用2 c: ", window.c);
+
+let root = document.createElement("button");
+root.textContent = "微应用 2 更改 history 为 micro2";
 document.body.appendChild(root);
 
-// this.a = 1;
-// console.log("微应用2 a: ", a);
-
-// var b = 2;
-// console.log("微应用2 b: ", window.b);
-
-// window.c = 3;
-// console.log("微应用2 c: ", window.c);
-
-// let root = document.createElement("button");
-// root.textContent = "微应用 2 更改 history 为 micro2";
-// document.body.appendChild(root);
-
-// root.onclick = () => {
-//   history.pushState({}, "", "/micro2");
-// };
+root.onclick = () => {
+  history.pushState({}, "", "/micro2");
+};
