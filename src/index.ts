afca1f3e59d6dec17d378259461d18260a46bd06
ziyi2 <18768107826@163.com>
2023-08-23 20:31:04 +0800

    feat: 优化示例

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from "./comm/comm1";
 export * from "./comm/comm2";
 export * from "./core/core";
-export * from "./nav/nav";
 export * from "./opt/opt1";
 export * from "./opt/opt2";
 export * from "./sandbox/sandbox1";
