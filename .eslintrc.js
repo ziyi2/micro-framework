2356144fd3653d602462f6c038a6960877def16d
ziyi2 <18768107826@163.com>
2023-09-12 19:32:44 +0800

    特性(工程设计): 新增 Jest 的 ESLint 配置
    Jest \& ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    'prettier'
+    "prettier",
+    // 新增 Jest 推荐的 ESLint 校验规则
+    // 所有规则集查看：https://github.com/jest-community/eslint-plugin-jest#rules（recommended 标识表明是推荐规则）
+    "plugin:jest/recommended",
+    "plugin:jest/style",
   ],
   // 解析器：将 TypeScript 的 AST 转换成兼容 ESLint 的 AST
   parser: "@typescript-eslint/parser",
@@ -17,5 +21,5 @@ module.exports = {
   // 插件：提供 TypeScript 校验规则的实现
   plugins: ["@typescript-eslint"],
   // 层叠配置：停止向上遍历 ESLint 配置文件
-  root: true
+  root: true,
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  // 注意和 package.json 中的 {  "lint": "eslint --ext .ts src" } 保持一致
+  // 注意和 package.json 中的 {  "lint": "eslint --ext .ts src test" } 保持一致
   "src/**/*.ts": "eslint",
+  "test/**/*.ts": "eslint",
 };
