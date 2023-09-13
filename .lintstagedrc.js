c0fcac6d9b8378aaeef396e262dfbc47e1c1a555
ziyi2 <18768107826@163.com>
2023-09-13 20:35:47 +0800

    特性(工程设计): 新增 Jest 的提交钩子,确保测试通过才能进行代码提交
    lint-staged for jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,6 @@ module.exports = {
   // 注意和 package.json 中的 {  "lint": "eslint --ext .ts src test" } 保持一致
   "src/**/*.ts": "eslint",
   "test/**/*.ts": "eslint",
+  // 新增暂存区代码的单元测试
+  "src/**/*.ts": "jest --bail --findRelatedTests",
 };
