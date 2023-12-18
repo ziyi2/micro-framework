451e14b3dbeb7132edca87e20030421702bf064e
ziyi2 <18768107826@163.com>
2023-12-18 19:45:10 +0800

    feat: 新增 single-spa 的 npm 引入示例

--- packages/main-app/src/index.js
+++ packages/main-app/src/index.js
@@ -11,13 +11,21 @@ import {
   mockMicroApps,
 } from "./utils/micros";
 
-// 根据后端提供的动态数据注册微应用
+// 对 single-spa 的注册 API 进行了二次封装，支持传入数组进行批量注册
 registerMicroApps(
+  // 根据后端提供的动态数据批量注册微应用
   mockMicroApps.map((item) => ({
     name: item.name,
     app: () => {
-      // import 无法使用变量，所以这里需要使用 if/else 判断
+      // import 无法使用变量，所以这里需要使用 if/else 判断进行硬编码
       if (item.router === MICRO_APP_ROUTER.REACT) {
+        // 按需动态加载微应用的 NPM 包（Webpack 会进行 chunk 分离）
+
+        // 注意 app 参数需要返回的是 Promise 对象
+        // 除此之外，使用 getAppLifeCycles 对需要返回的周期函数进行 async 处理
+        // 这里可以重点再回顾一下注册微应用 API 的参数声明
+        // 1、app 本身如果是函数，那么必须是 async 函数（需要返回 Promise 对象）
+        // 2、Promise.resolve 需要返回生命周期函数对象，每一个生命周期函数也必须是 async 函数
         return import("react-micro-app").then((res) => getAppLifeCycles(res));
       } else if (item.router === MICRO_APP_ROUTER.VUE) {
         return import("vue-micro-app").then((res) => getAppLifeCycles(res));
@@ -25,6 +33,7 @@ registerMicroApps(
     },
     activeWhen: item.router,
     customProps: {
+      // 向微应用传递需要挂载的容器元素 ID
       container: MICRO_APP_CONTAINER_ID,
     },
   }))
@@ -39,6 +48,7 @@ const router = createBrowserRouter([
     // <Outlet> 是 react-router-dom 提供的一个组件，用于渲染子路由：https://reactrouter.com/en/main/components/outlet
 
     // 遍历迭代 mockMicroApps 中的数据，生成对应的路由配置
+    // 这里的路由配置和微应用注册 API 中的 activeWhen 一致
     children: mockMicroApps.map((item) => ({
       path: item.router,
       // 微应用的容器元素，用于渲染微应用
--- packages/main-app/src/utils/single-spa.ts
+++ packages/main-app/src/utils/single-spa.ts
@@ -1,8 +1,9 @@
+// 使用 Lerna 执行 lerna bootstrap 之后，本地 packages 中的应用可以通过 NPM 包的方式进行引入
 // 注意这里直接引入了开发态的入口文件，而不是打包后的文件
 import { start, registerApplication } from "single-spa/src/single-spa";
 import { RegisterApplicationConfig } from "single-spa";
 
-// 对 single-spa 的 registerApplication 进行二次封装，使其可以接收一个数组，批量注册子应用
+// 对 single-spa 的 registerApplication 进行二次封装，使其可以接收一个数组，批量注册微应用
 export function registerMicroApps(apps: RegisterApplicationConfig[]) {
   // @ts-ignore
   // 如果不开启 __DEV__，single-spa 无法正常运行
@@ -11,12 +12,15 @@ export function registerMicroApps(apps: RegisterApplicationConfig[]) {
   start();
 }
 
+// 对 single-spa 注册 API 的 app 参数需要的生命周期函数对象进行适配
+// 为了使得微应用中导出的周期函数不需要在顶层作用域提供 async
+// 需要在主应用中对周期函数进行 async 包装处理
 export function getAppLifeCycles(res) {
   let lifeCycles = {};
   Object.keys(res).forEach((key) => {
     const lifecycle = res[key];
     if (lifecycle && typeof lifecycle === "function") {
-      // 将子应用的所有 lifeCycle 函数进行包装，包装成返回 Promise 的
+      // 将子应用的所有 lifeCycle 函数进行包装，包装成 async 函数
       lifeCycles[key] = async (...args) => {
         console.log(`[main-app] ${key} start`);
         await lifecycle(...args);
