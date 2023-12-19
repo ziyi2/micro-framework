d929be605a82699d36108835276bada6169e9356
ziyi2 <18768107826@163.com>
2023-12-19 20:09:05 +0800

    feat: 新增 single-spa 的 npm 引入示例

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -6,19 +6,35 @@ import App from "./App";
 
 let root;
 
+// 判断是否在 single-spa 的环境中运行
+// 如果不是，那么说明不在微前端的环境中，可以独立启动
+if (!window.singleSpaNavigate) {
+  root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
 /**
- * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
- * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
+ * bootstrap 只会在微应用首次激活时调用一次
+ * 微应用重新激活时会直接调用 mount 周期函数，不会再重复触发 bootstrap。
+ * 通常情况下这里不需要进行任何处理，初始化的代码可以放在当前入口文件的顶层处理
+ * 除非会执行微应用的 unload 动作，然后需要再次执行 bootstrap，可能需要在此做一些必要的初始化动作
+ * 注意这里的周期函数不是 async 函数，在主应用中引入该周期函数后需要进行 async 处理
  */
 export function bootstrap() {
   console.log("[React 子应用] bootstrap excuted");
 }
 
 /**
- * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
+ * 微应用每次激活时都会调用 mount 周期函数，通常在这里执行微应用的渲染
  */
 export function mount(props) {
   console.log("[React 子应用] mount excuted, props: ", props);
+  // 在 single-spa 的注册 API 中会通过 customProps 传递 container 微应用容器元素 ID
+  // 因此这里将微应用挂载在主应用的容器元素上
   root = ReactDOM.createRoot(document.getElementById(props.container));
   root.render(
     <React.StrictMode>
@@ -28,7 +44,7 @@ export function mount(props) {
 }
 
 /**
- * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
+ * 微应用每次失活时会调用 unmount 周期函数，通常在这里执行微应用的卸载
  */
 export function unmount(props) {
   console.log("[React 子应用] unmount excuted, props: ", props);
--- packages/vue-app/src/main.js
+++ packages/vue-app/src/main.js
@@ -2,26 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 let app;
 
-/**
- * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
- * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
- */
 export function bootstrap() {
   console.log("[Vue 子应用] bootstrap excuted");
 }
 
-/**
- * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
- */
 export function mount(props) {
   console.log("[Vue 子应用] mount excuted, props: ", props);
   app = createApp(App);
   app.mount(`#${props.container}`);
 }
 
-/**
- * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
- */
 export function unmount(props) {
   console.log("[Vue 子应用] unmount excuted, props: ", props);
   app && app.unmount();
