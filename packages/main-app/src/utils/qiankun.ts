66ab691b4cf06591d5280f45d9826e1eb9e628cf
ziyi2 <18768107826@163.com>
2024-08-25 11:08:22 +0800

    feat: CSS 无沙箱示例

--- packages/main-app/src/utils/qiankun.ts
+++ packages/main-app/src/utils/qiankun.ts
@@ -4,12 +4,15 @@ export function registerQiankunMicroApps(
   microApps: RegistrableApp<{ microContainer: string }>[]
 ) {
   registerMicroApps(microApps);
+  // start({
+  //   sandbox: {
+  //     // 开启 Shadow DOM 沙箱
+  //     // strictStyleIsolation: true,
+  //     // 增加一个特殊的选择器规则来限定其影响范围
+  //     experimentalStyleIsolation: true,
+  //   },
+  // });
   start({
-    sandbox: {
-      // 开启 Shadow DOM 沙箱
-      // strictStyleIsolation: true,
-      // 增加一个特殊的选择器规则来限定其影响范围
-      experimentalStyleIsolation: true,
-    },
+    sandbox: false,
   });
 }
