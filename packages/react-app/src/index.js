4d15b1a8c3d06fe5e3f8b6a28d5a8e69c0f88dd8
ziyi2 <18768107826@163.com>
2024-12-30 19:42:51 +0800

    feat:  Framework 状态设计

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -30,9 +30,8 @@ export async function mount(props) {
   // 变更 window 属性
   window.micro = "micro-react";
 
-  // qiankun 在注册 react 子应用时会通过 props 传递 container
+  // micro-framework 在注册 react 子应用时会通过 props 传递 container
   // Creact React App 自带的 HTML 模版的挂载节点是 #root（可以查看 public/index.html）
-  // 由于微应用的 HTML 内容会挂载在 container 上
   // 因此可以从 container 中获取到 #root 节点挂载 react 应用
   root = ReactDOM.createRoot(props.container.querySelector("#root"));
   root.render(<App />);
