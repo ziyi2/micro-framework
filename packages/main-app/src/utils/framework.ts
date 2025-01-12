040497a1d101decaaa4383d47e38859a651d7c21
ziyi2 <18768107826@163.com>
2025-01-12 09:44:12 +0800

    feat: 新增 Pretch 预加载

--- packages/main-app/src/utils/framework.ts
+++ packages/main-app/src/utils/framework.ts
@@ -3,16 +3,13 @@ import { start, registerMicroApps, IRegisterApp } from "micro-framework";
 export function registerFrameworkMicroApps(microApps: IRegisterApp[]) {
   registerMicroApps(microApps);
   start({
-    sandbox: true,
     // prefetch: ["react"],
-    prefetch: true,
+    prefetch: "all",
     // prefetch: ["react", "vue"],
-
     // 此时如果激活了某个微应用
     // 微应用的预加载和自定义加载同时触发执行
     // 不会重复加载微应用资源，因为 import-html-entry 会对加载的微应用资源的 fetch Promise 进行了缓存
     // prefetch: "all",
-
     // prefetch: (apps: IRegisterApp[]) => {
     //   return {
     //     criticalAppNames: ["react"],
